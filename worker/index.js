807ffeb6657319b19843c44a4750be6db584e45e
Beth-A <elizabeth78@me.com>
2020-08-31 17:16:42 -0400

    link docker containers to aws elastic beanstalk

--- worker/index.js
+++ worker/index.js
@@ -2,18 +2,20 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
+    host: keys.redisHost,
+    port: keys.redisPort,
+    // tells redis to try to reconnect every 1sec if connection is lost
+    retry_strategy: () => 1000
 });
 const sub = redisClient.duplicate();
 
 function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)));
+sub .on('message', (channel, message) => {
+    redisClient.hset('values', message, fib(parseInt(message)));
 });
+
 sub.subscribe('insert');
